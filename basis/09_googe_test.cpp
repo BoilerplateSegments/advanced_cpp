003265b41558ef5cc0670be47ee599b230daec5a
shenleban tongying <shenlebantongying@gmail.com>
2021-04-16 04:04:17 -0400

    add a minimum google test

---
+++ basis/09_googe_test.cpp
@@ -0,0 +1,5 @@
+#include "09_googe_test.h"
+
+int slb_double(int x){
+  return x*x;
+}
---
+++ test/09_test.cpp
@@ -0,0 +1,6 @@
+#include "09_googe_test.h"
+#include "gtest/gtest.h"
+
+TEST(mytest,test1){
+  EXPECT_EQ (slb_double(2),4);
+}
