59854bd8cc82b70489a6e5cabaa7676dc7f1a119
shenleban tongying <shenlebantongying@gmail.com>
2021-05-10 17:45:22 -0400

    Autolize new file

--- basis_decoupled/00_template.cpp
+++ basis_decoupled/00_template.cpp
@@ -1,4 +1,4 @@
 #include <iostream>
 int slbplus(int x, int y) {
-  std::cout<<"nce";
-  return x+y; }
+  return x+y;
+}
---
+++ basis_decoupled/00_template_test.cpp
@@ -0,0 +1,8 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "00_template.cpp"
+
+#include "doctest.h"
+
+TEST_CASE("testing template -> 00") {
+  CHECK(slbplus(1,2) == 3);
+}
